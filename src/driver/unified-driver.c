428c04f4d6ef91e8b0cf8e32b615065729f6e343
N. Everett Howard <howard@nth.blue>
2025-07-20 14:17:39 -0400

    Fix timestep bugs causing calculation not to be representative of time cost.

--- src/driver/unified-driver.c
+++ src/driver/unified-driver.c
@@ -23,10 +23,12 @@ int unified_driver(struct simulation_properties sim_prop,
     mesh->h = 10e+12;
     mesh->volume = 1;
     float t_end = sim_prop.t_end;
-    mesh->dt = t_end / 100000;
+    // float dt = t_end / 100000;
+    float dt = t_end / 1000;
     float t = 0;
     float tres = sim_prop.output_tres;
-    float t_inter = t_end / tres;
+    float t_inter_lvl = t_end / tres;
+    float t_inter = 0;
     float base_temp = sim_prop.hydro_temp_base;
     float base_density = sim_prop.hydro_density_base;
 
@@ -37,6 +39,7 @@ int unified_driver(struct simulation_properties sim_prop,
         mesh->dim[0] = sim_prop.resolution[0];
         mesh->dim[1] = sim_prop.resolution[1];
         mesh->dim[2] = sim_prop.resolution[2];
+        mesh->dt = dt;
 
         mesh->volume = mesh->volume / mesh->dim[0];
         mesh->volume = mesh->volume / mesh->dim[1];
@@ -77,6 +80,7 @@ int unified_driver(struct simulation_properties sim_prop,
             }
         }
         thermo[0][0][0] = network_create(options);
+        thermo[0][0][0]->f->dt_init = 1e-17;
         for (int i = 0; i < sim_prop.resolution[0]; i++) {
             for (int j = 0; j < sim_prop.resolution[1]; j++) {
                 for (int k = 0; k < sim_prop.resolution[2]; k++) {
@@ -108,7 +112,7 @@ int unified_driver(struct simulation_properties sim_prop,
         neutrino[0][0][0]->f->g_a = 7.5e-01;
         neutrino[0][0][0]->f->g_b = 1.0e+02;
         neutrino[0][0][0]->f->g_c = sqrt(50.0);
-        neutrino[0][0][0]->f->dt = mesh->dt;
+        neutrino[0][0][0]->f->dt = 1e-15;
         neutrino[0][0][0]->f->dt_new = neutrino[0][0][0]->f->dt;
         for (int i = 0; i < sim_prop.resolution[0]; i++) {
             for (int j = 0; j < sim_prop.resolution[1]; j++) {
@@ -142,69 +146,78 @@ int unified_driver(struct simulation_properties sim_prop,
 
     struct problem_parameters* params;
     while (t < t_end) {
-        mesh->t_end = t_inter;
-        if (sim_prop.hydro) {
-            if (hydro_integrate_rt_mesh(mesh, options) == EXIT_FAILURE) {
-                rt_hydro_mesh_destroy(&mesh);
-                return EXIT_FAILURE;
+        t_inter += t_inter_lvl;
+        while (t < t_inter) {
+            // If the mesh adjusts the timestep.
+            dt = mesh->dt;
+            mesh->t_end = 0; // Single step
+            if (sim_prop.hydro) {
+                if (hydro_integrate_rt_mesh(mesh, options) == EXIT_FAILURE) {
+                    rt_hydro_mesh_destroy(&mesh);
+                    return EXIT_FAILURE;
+                }
             }
-        }
-        if (sim_prop.neutrino) {
-            for (int i = 0; i < sim_prop.resolution[0]; i++) {
-                for (int j = 0; j < sim_prop.resolution[1]; j++) {
-                    for (int k = 0; k < sim_prop.resolution[2]; k++) {
-                        // Hydro ==> Neutrino
-                        neutrino[i][j][k]->f->kt =
-                            mesh->temp[i][j][k] * BOLTZMANN_CONSTANT;
-                        neutrino[i][j][k]->f->rho = mesh->density[i][j][k];
-                        // TODO: regenerate certain params that change with
-                        // this.
-                        if (neunet_integrate_network(neutrino[i][j][k],
-                                                     options) == EXIT_FAILURE) {
-                            return EXIT_FAILURE;
+            if (sim_prop.neutrino) {
+                for (int i = 0; i < sim_prop.resolution[0]; i++) {
+                    for (int j = 0; j < sim_prop.resolution[1]; j++) {
+                        for (int k = 0; k < sim_prop.resolution[2]; k++) {
+                            // Hydro ==> Neutrino
+                            neutrino[i][j][k]->f->kt =
+                                mesh->temp[i][j][k] * BOLTZMANN_CONSTANT;
+                            neutrino[i][j][k]->f->rho = mesh->density[i][j][k];
+                            neutrino[i][j][k]->f->t_end = dt;
+                            // TODO: regenerate certain params that change with
+                            // this.
+                            if (neunet_integrate_network(neutrino[i][j][k],
+                                                         options) ==
+                                EXIT_FAILURE) {
+                                return EXIT_FAILURE;
+                            }
+                            // TODO: Retrieve values from neutrino.
+                            // Neutrino ==> Hydro
                         }
-                        // TODO: Retrieve values from neutrino.
-                        // Neutrino ==> Hydro
                     }
                 }
             }
-        }
-        if (sim_prop.thermo) {
-            for (int i = 0; i < sim_prop.resolution[0]; i++) {
-                for (int j = 0; j < sim_prop.resolution[1]; j++) {
-                    for (int k = 0; k < sim_prop.resolution[2]; k++) {
-                        // Hydro ==> Thermo
-                        thermo[i][j][k]->f->rho = mesh->density[i][j][k];
-                        thermo[i][j][k]->f->t9 = mesh->temp[i][j][k] / 1e9;
-                        float density[3];
-                        density[0] = 1.0f;
-                        density[1] = thermo[i][j][k]->f->rho;
-                        density[2] =
-                            thermo[i][j][k]->f->rho * thermo[i][j][k]->f->rho;
-                        for (int n = 0; n < rates->number_reactions; n++) {
-                            rates->prefactor[n] *=
-                                (density[rates->num_react_species[n] - 1]);
+            if (sim_prop.thermo) {
+                for (int i = 0; i < sim_prop.resolution[0]; i++) {
+                    for (int j = 0; j < sim_prop.resolution[1]; j++) {
+                        for (int k = 0; k < sim_prop.resolution[2]; k++) {
+                            // Hydro ==> Thermo
+                            thermo[i][j][k]->f->rho = mesh->density[i][j][k];
+                            thermo[i][j][k]->f->t9 = mesh->temp[i][j][k] / 1e9;
+                            float density[3];
+                            density[0] = 1.0f;
+                            density[1] = thermo[i][j][k]->f->rho;
+                            density[2] = thermo[i][j][k]->f->rho *
+                                         thermo[i][j][k]->f->rho;
+                            thermo[i][j][k]->f->t_max = dt;
+                            for (int n = 0; n < rates->number_reactions; n++) {
+                                rates->prefactor[n] *=
+                                    (density[rates->num_react_species[n] - 1]);
+                            }
+                            params = problem_parameters_create(
+                                rates, thermo[i][j][k], options);
+                            if (tnn_integrate_network(rates, thermo[i][j][k],
+                                                      params, options) ==
+                                EXIT_FAILURE) {
+                                return EXIT_FAILURE;
+                            }
+                            problem_parameters_destroy(
+                                &params, thermo[i][j][k]->info->number_species);
                         }
-                        params = problem_parameters_create(
-                            rates, thermo[i][j][k], options);
-                        if (tnn_integrate_network(rates, thermo[i][j][k],
-                                                  params,
-                                                  options) == EXIT_FAILURE) {
-                            return EXIT_FAILURE;
-                        }
-                        problem_parameters_destroy(
-                            &params, thermo[i][j][k]->info->number_species);
+                        // Thermo ==> Hydro
+                        // TODO: Retrieve values from thermo.
                     }
-                    // Thermo ==> Hydro
-                    // TODO: Retrieve values from thermo.
                 }
             }
+            if (sim_prop.output && sim_prop.hydro) {
+                fprint_float_3d(sim_prop.hydro_out_file, mesh->temp,
+                                mesh->dim[0], mesh->dim[1], mesh->dim[2]);
+            }
+
+            t += dt;
         }
-        if (sim_prop.output && sim_prop.hydro) {
-            fprint_float_3d(sim_prop.hydro_out_file, mesh->temp, mesh->dim[0],
-                            mesh->dim[1], mesh->dim[2]);
-        }
-        t += t_inter;
         printf("\x1b[1A\x1b[2K\x1b[0G  Time: [%6.2f/%6.2f]\n", t, t_end);
     }
     kerneltime = clock() - kerneltime;
